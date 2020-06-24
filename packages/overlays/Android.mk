36d79911933df951c9a2442a1403090a9b773d14
Andrew Thomas <itz63c@statixos.com>
2020-06-24 17:38:16 -0400

    overlays: add new icon shapes from Android R
    extracted from taimen-user 11 RPB1.200504.018 6520161 release-keys
    
    decompiled with apktool & refactored
    
    Change-Id: I803f2532e79e383223647bf90e53c77f5f0f69f4
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/2695
    Reviewed-by: Andrew Thomas <itz63c@statixos.com>
    Tested-by: Andrew Thomas <itz63c@statixos.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -110,6 +110,9 @@ LOCAL_REQUIRED_MODULES := \
 	IconShapeRoundedRectOverlay \
 	IconShapeSquircleOverlay \
 	IconShapeTeardropOverlay \
+	IconShapePebbleOverlay \
+	IconShapeVesselOverlay \
+	IconShapeTaperedRectOverlay \
 	NavigationBarMode3ButtonOverlay \
 	NavigationBarMode2ButtonOverlay \
 	NavigationBarModeGesturalOverlay \
